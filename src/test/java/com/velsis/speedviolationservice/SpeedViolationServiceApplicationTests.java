9e5924051c0e9df013635f800b3b70b8d831eb96
Vinicius Buzzacaro <ude5xrqy@anonaddy.com>
2026-08-08 13:30:28 -0300

    first commit

---
+++ src/test/java/com/velsis/speedviolationservice/SpeedViolationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velsis.speedviolationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedViolationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
